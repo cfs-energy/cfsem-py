61dac4f5a5ffaaba0f036a79755285a7e6d351fd
James Logan <jlogan@cfs.energy>
2025-10-29 10:52:02 -0400

    format

--- src/physics/point_source.rs
+++ src/physics/point_source.rs
@@ -41,7 +41,11 @@ pub fn flux_density_dipole_scalar(
 
     // r(dot(m, r))/|r|^5 reordered to avoid computing the 5th power for improved float resolution
     let m_dot_rhat = dot3(moment.0, moment.1, moment.2, rhat.0, rhat.1, rhat.2);
-    let rmr = (rhat.0 * m_dot_rhat, rhat.1 * m_dot_rhat, rhat.2 * m_dot_rhat);
+    let rmr = (
+        rhat.0 * m_dot_rhat,
+        rhat.1 * m_dot_rhat,
+        rhat.2 * m_dot_rhat,
+    );
 
     // Assemble components
     let c = 3.0 * rinv3;

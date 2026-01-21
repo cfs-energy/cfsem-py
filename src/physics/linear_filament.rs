612a74701ae530d8d645e5290b39f76e7c14769a
James Logan <jlogan@cfs.energy>
2026-01-21 15:14:51 -0500

    test vector potential against point-segment discretization

--- src/physics/linear_filament.rs
+++ src/physics/linear_filament.rs
@@ -741,7 +741,10 @@ mod test {
     use std::f64::consts::PI;
 
     use super::*;
-    use crate::physics::point_source::segment::flux_density_point_segment;
+    use crate::physics::point_source::segment::{
+        flux_density_point_segment,
+        vector_potential_point_segment,
+    };
     use crate::testing::*;
 
     /// Make sure the forces have the right sign
@@ -942,6 +945,109 @@ mod test {
                 "bz is {}, should be {}",
                 bz[i],
                 bz_ps[i]
+            );
+        }
+    }
+
+    /// Compare single-segment vector potential against discretized point-source segments.
+    #[test]
+    fn test_vector_potential_against_point_segment_discretization() {
+        let (rtol, atol) = (1e-6, 1e-15);
+
+        let start = (0.0, 0.0, -0.5);
+        let end = (0.0, 0.0, 0.5);
+        let ifil = [1.0];
+
+        let xfil = [start.0];
+        let yfil = [start.1];
+        let zfil = [start.2];
+        let dlx = [end.0 - start.0];
+        let dly = [end.1 - start.1];
+        let dlz = [end.2 - start.2];
+        let xyzfil = (&xfil[..], &yfil[..], &zfil[..]);
+        let dlxyz = (&dlx[..], &dly[..], &dlz[..]);
+
+        let ngrid = 10;
+        let span = 10.0;
+        let xvals: Vec<f64> = (0..ngrid)
+            .map(|i| -span + (2.0 * span) * (i as f64) / (ngrid as f64 - 1.0))
+            .collect();
+        let yvals = xvals.clone();
+        let zvals = xvals.clone();
+
+        let total = ngrid * ngrid * ngrid;
+        let mut xp = Vec::with_capacity(total);
+        let mut yp = Vec::with_capacity(total);
+        let mut zp = Vec::with_capacity(total);
+        for &x in &xvals {
+            for &y in &yvals {
+                for &z in &zvals {
+                    xp.push(x);
+                    yp.push(y);
+                    zp.push(z);
+                }
+            }
+        }
+        let xyzp = (&xp[..], &yp[..], &zp[..]);
+
+        let mut ax = vec![0.0; total];
+        let mut ay = vec![0.0; total];
+        let mut az = vec![0.0; total];
+        vector_potential_linear_filament(
+            xyzp,
+            xyzfil,
+            dlxyz,
+            &ifil,
+            &[0.0],
+            (&mut ax, &mut ay, &mut az),
+        )
+        .unwrap();
+
+        let nseg = 1000;
+        let dz = (end.2 - start.2) / nseg as f64;
+        let mut xfil_ps = Vec::with_capacity(nseg);
+        let mut yfil_ps = Vec::with_capacity(nseg);
+        let mut zfil_ps = Vec::with_capacity(nseg);
+        for i in 0..nseg {
+            xfil_ps.push(start.0);
+            yfil_ps.push(start.1);
+            zfil_ps.push(start.2 + dz * i as f64);
+        }
+        let dlx = vec![0.0; nseg];
+        let dly = vec![0.0; nseg];
+        let dlz = vec![dz; nseg];
+        let ifil_ps = vec![1.0; nseg];
+
+        let mut ax_ps = vec![0.0; total];
+        let mut ay_ps = vec![0.0; total];
+        let mut az_ps = vec![0.0; total];
+        vector_potential_point_segment(
+            xyzp,
+            (&xfil_ps, &yfil_ps, &zfil_ps),
+            (&dlx, &dly, &dlz),
+            &ifil_ps,
+            (&mut ax_ps, &mut ay_ps, &mut az_ps),
+        )
+        .unwrap();
+
+        for i in 0..xp.len() {
+            assert!(
+                approx(ax[i], ax_ps[i], rtol, atol),
+                "ax is {}, should be {}",
+                ax[i],
+                ax_ps[i]
+            );
+            assert!(
+                approx(ay[i], ay_ps[i], rtol, atol),
+                "ay is {}, should be {}",
+                ay[i],
+                ay_ps[i]
+            );
+            assert!(
+                approx(az[i], az_ps[i], rtol, atol),
+                "az is {}, should be {}",
+                az[i],
+                az_ps[i]
             );
         }
     }

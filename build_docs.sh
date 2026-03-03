15a7b9ac0fce1238956fbf5753891d309fef37b3
James Logan <jlogan@cfs.energy>
2026-03-03 15:20:24 -0500

    set env var when running examples for docs build

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Run the python examples that generate figures for the documentation.
+export CFSEM_TESTING="True";
 (cd examples && for f in *.py; do python "$f"; done)
 cp examples/*.png docs/python/example_outputs
 

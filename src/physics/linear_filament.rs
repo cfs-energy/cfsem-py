88b5d6d80aaeb2fd5179bb6847852118788750d9
James Logan <jlogan@cfs.energy>
2026-03-05 10:55:00 -0500

    more commentary about the gauge shift

--- src/physics/linear_filament.rs
+++ src/physics/linear_filament.rs
@@ -568,14 +568,21 @@ pub fn vector_potential_linear_filament_scalar(
     //
     // The field shape inside the conductor is handled later; this separation
     // is necessary due to the discontinuity in the J-field.
-    let k1 = -para_b + dist_b;
-    let k2 = -para_a + dist_a;
-    let a_edge = MU0_OVER_4PI * ifil * libm::log((k1 / k2).max(0.0));
+    let k1 = -para_b + dist_b; // (m)
+    let k2 = -para_a + dist_a; // (m)
+    let a_edge = MU0_OVER_4PI * ifil * libm::log((k1 / k2).max(0.0)); // (V-s/m)
 
     // Finite-thickness effect for points inside the conductor or near the endpoints.
     //
-    // Outside the conductor, the gauge is already consistent with both curl(A)=B
+    // Because the B-field is the derivative of the A-field, where the B-field falls off
+    // linearly inside the conductor, the A-field falls off quadratically.
+    // This constrains the _form_ of the variation of A inside the conductor, but leaves
+    // a free integration constant known as the gauge.
+    //
+    // Outside the conductor, the gauge is already chosen consistent with both curl(A)=B
     // and with the evaluation of mutual inductances and voltages due to dA/dt.
+    // This is easy because it is a constant outside the conductor, where the enclosed
+    // current is not changing.
     //
     // However, inside the conductor, some extra attention to the gauge is needed
     // in order to ensure that the field is both continuous at the edge and
@@ -584,17 +591,18 @@ pub fn vector_potential_linear_filament_scalar(
     // Here, we use a gauge shift (kappa, the factor shared with the B-field calc) to
     // ensure the the A-field inside the conductor is consistent with curl(A)=B
     // both inside and outside the conductor.
-    let frac2 = frac * frac; // Quadratic fall-off (as opposed to linear for B-field)
-    let a_mag = a_edge + 0.5 * kappa * (1.0 - frac2);
+    let frac2 = frac * frac; // (dimensionless) Quadratic fall-off (vs. linear for B-field)
+    let a_mag = a_edge + 0.5 * kappa * (1.0 - frac2); // (V-s/m) Gauge shift
 
     // Direction is always aligned with the segment.
+    // (V-s)
     let (ax, ay, az) = (a_mag * dlhat.0, a_mag * dlhat.1, a_mag * dlhat.2);
 
     // Finally, determine whether we are clipping to zero.
     if perp > MIN_WIRE_THICKNESS {
-        return (ax, ay, az);
+        return (ax, ay, az); // (V-s)
     } else {
-        return (0.0, 0.0, 0.0);
+        return (0.0, 0.0, 0.0); // (V-s)
     }
 }
 
